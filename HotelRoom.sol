d4442ec089f842cb351c1e2a07b814d30f19c046
Christian Ortel <82163209+christianortel@users.noreply.github.com>
2022-06-09 12:26:26 -0400

    Create HotelRoom.sol

---
+++ HotelRoom.sol
@@ -0,0 +1,39 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract HotelRoom {
+    enum Statuses {
+        Vacant,
+        Occupied
+    }
+    Statuses public currentStatus;
+
+    event Occupy(address _occupant, uint _value);
+
+    address payable owner;
+
+    constructor() {
+        owner = payable(msg.sender);
+        currentStatus = Statuses.Vacant;
+    }
+
+    modifier onlyWhileVacant {
+        require(currentStatus == Statuses.Vacant, "Currently occupied.");
+        _;
+    }
+
+    modifier costs(uint _amount) {
+        require(msg.value >= _amount, "Not enough ether provided.");
+        _;
+    }
+
+    function book() public payable onlyWhileVacant costs(2 ether) {
+        currentStatus = Statuses.Occupied;
+
+        (bool sent, bytes memory data) = owner.call{value: msg.value}("");
+        require(sent);
+
+        emit Occupy(msg.sender, msg.value);
+    }
+
+}
